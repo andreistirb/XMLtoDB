f83b620c3e707a91fecad2cce498b8a1dabdfddd
Andrei <andreistirb@yahoo.com>
2015-07-30 18:32:52 +0300

    show trackpoints

--- src/com/andreistirb/xmltodb/Main.java
+++ src/com/andreistirb/xmltodb/Main.java
@@ -9,6 +9,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
@@ -22,19 +23,38 @@ public class Main {
 		s.close();
 		System.out.println(filename);
 		File file = new File(filename);
-		
-		
+
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder = factory.newDocumentBuilder();
 		Document document = builder.parse(file);
-		
-		NodeList nodeList = document.getDocumentElement().getChildNodes();
-		
-		for(int i=0; i<nodeList.getLength();i++){
-			Node node = nodeList.item(i);
-			System.out.println(node.getNodeName());
+
+		NodeList trkList = document.getDocumentElement().getChildNodes();
+
+		for (int i = 0; i < trkList.getLength(); i++) {
+			Node trk = trkList.item(i);
+			if (trk.getNodeName().equals("trk")) {
+				NodeList trksegList = trk.getChildNodes();
+				for (int j = 0; j < trksegList.getLength(); j++) {
+					Node trkseg = trksegList.item(j);
+					if (trkseg.getNodeName().equals("trkseg")) {
+						NodeList trkptList = trkseg.getChildNodes();
+						for (int k = 0; k < trkptList.getLength(); k++) {
+							Node trkpt = trkptList.item(k);
+							if (trkpt instanceof Element) {
+								System.out.println(
+										trkpt.getNodeName() 
+												+ "		"
+												+ trkpt.getAttributes().getNamedItem("lat").getNodeValue()
+												+ "     "
+												+ trkpt.getAttributes().getNamedItem("lon").getNodeValue());
+							}
+						}
+					}
+
+				}
+			}
 		}
-		
+
 	}
 
 }
