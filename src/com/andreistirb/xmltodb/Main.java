79ad8aa49a5a8c4c2ec1e2d244bb69da5464e8c7
Andrei <andreistirb@yahoo.com>
2015-07-30 18:11:32 +0300

    parse xml file

--- src/com/andreistirb/xmltodb/Main.java
+++ src/com/andreistirb/xmltodb/Main.java
@@ -1,15 +1,40 @@
 package com.andreistirb.xmltodb;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
 		String filename = s.next();
 		s.close();
 		System.out.println(filename);
+		File file = new File(filename);
+		
+		
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		Document document = builder.parse(file);
+		
+		NodeList nodeList = document.getDocumentElement().getChildNodes();
+		
+		for(int i=0; i<nodeList.getLength();i++){
+			Node node = nodeList.item(i);
+			System.out.println(node.getNodeName());
+		}
+		
 	}
 
 }
--- src/com/andreistirb/xmltodb/Track.java
+++ src/com/andreistirb/xmltodb/Track.java
@@ -5,4 +5,21 @@ import java.util.ArrayList;
 public class Track {
 	private String title;
 	private ArrayList<TrackPoint> points;
+	
+	public Track(){
+		this.title = "";
+		this.points = new ArrayList<TrackPoint>();
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void addPoint(TrackPoint tp){
+		this.points.add(tp);
+	}
 }
