85e3b56726c09b80674d7c7fbf0b325e3b7052e1
Andrei <andreistirb@yahoo.com>
2015-08-05 12:16:42 +0300

    improve insert time

--- src/com/andreistirb/xmltodb/Database.java
+++ src/com/andreistirb/xmltodb/Database.java
@@ -9,68 +9,53 @@ public class Database {
 
 	Connection connection;
 	Statement stmt;
-	
-	public Database(String dbName){
-	
+
+	public Database(String dbName) {
+
 		connection = null;
-		try{
+		try {
 			Class.forName("org.sqlite.JDBC");
 			connection = DriverManager.getConnection("jdbc:sqlite:" + dbName);
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
 			System.exit(0);
 		}
-		
+
 	}
-	
-	private void insertTrack(Track traseu){
-		
-		try {
+
+	private void insertTrack(Track traseu) throws SQLException {
 			stmt = connection.createStatement();
-			String sql = "INSERT INTO Tracks(track_id, mountain_id,"
-					+ "track_name, track_difficulty, track_mark, "
-					+ "track_length, track_description, track_availability) " + 
-			"VALUES(" 
-					+ Integer.parseInt(traseu.getId()) + ", "
-					+ Integer.parseInt(traseu.getMountainId()) + ", "
-					+ "'" + traseu.getTitle() + "'" + ", "
-					+ "'" + traseu.getDifficulty() + "'" + ", "
-					+ "'" + traseu.getMark() + "'" + ", "
-					+ "'" + traseu.getDuration() + "'" + ", "
-					+ "'" + traseu.getDescription() + "'" + ", "
-					+ "'" + traseu.getAccesibility() + "'" + ")";
+			String sql = "INSERT INTO Tracks(track_id, mountain_id," + "track_name, track_difficulty, track_mark, "
+					+ "track_length, track_description, track_availability) " + "VALUES("
+					+ Integer.parseInt(traseu.getId()) + ", " + Integer.parseInt(traseu.getMountainId()) + ", " + "'"
+					+ traseu.getTitle() + "'" + ", " + "'" + traseu.getDifficulty() + "'" + ", " + "'"
+					+ traseu.getMark() + "'" + ", " + "'" + traseu.getDuration() + "'" + ", " + "'"
+					+ traseu.getDescription() + "'" + ", " + "'" + traseu.getAccesibility() + "'" + ")";
 			stmt.execute(sql);
+	}
+
+	private void insertTrackPoint(TrackPoint trackpoint, int order) throws SQLException {
+		stmt = connection.createStatement();
+		String sql = "INSERT INTO Trackpoints(track_id, " + "trackpoint_lat, trackpoint_lon, trackpoint_alt,"
+				+ "trackpoint_order)" + "VALUES(" + Integer.parseInt(trackpoint.getTrackId()) + ", "
+				+ Double.parseDouble(trackpoint.getLatitude()) + ", " + Double.parseDouble(trackpoint.getLongitude())
+				+ ", " + Double.parseDouble(trackpoint.getAltitude()) + ", " + order + ")";
+		stmt.execute(sql);
+
+	}
+
+	public void insertTrackIntoDatabase(Track traseu) {
+		try {
+			insertTrack(traseu);
+			stmt = connection.createStatement();
+			stmt.execute("begin transaction");
+			for (int i = 0; i < traseu.getPoints().size(); i++) {
+				insertTrackPoint(traseu.getPoints().get(i), i);
+			}
+			stmt.execute("end transaction");
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-	
-	private void insertTrackPoint(TrackPoint trackpoint, int order){
-		try {
-			stmt = connection.createStatement();
-			String sql = "INSERT INTO Trackpoints(track_id, "
-					+ "trackpoint_lat, trackpoint_lon, trackpoint_alt,"
-					+ "trackpoint_order)" + 
-					"VALUES("
-					+ Integer.parseInt(trackpoint.getTrackId()) + ", "
-					+ Double.parseDouble(trackpoint.getLatitude()) + ", "
-					+ Double.parseDouble(trackpoint.getLongitude()) + ", "
-					+ Double.parseDouble(trackpoint.getAltitude()) + ", "
-					+ order + ")";
-			stmt.execute(sql);
-		} catch (SQLException e){
 			e.printStackTrace();
 		}
 	}
-	
-	public void insertTrackIntoDatabase(Track traseu){
-		insertTrack(traseu);
-		for(int i=0;i<traseu.getPoints().size();i++){
-			insertTrackPoint(traseu.getPoints().get(i),i);
-		}
-	}
-	
+
 }
--- src/com/andreistirb/xmltodb/Main.java
+++ src/com/andreistirb/xmltodb/Main.java
@@ -1,39 +1,43 @@
 package com.andreistirb.xmltodb;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class Main {
 
 	public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
-		// TODO Auto-generated method stub
+		
 		Scanner s = new Scanner(System.in);
-		String filename = s.next();
+		String trackFile = s.next();
 		s.close();
-		
-		File file = new File(filename);
-		Track traseu = new Track();
-		XMLParser parser = new XMLParser(file);
-		Database db = new Database("MountainTracker.s3db");
-		
-		parser.parse(traseu);
-		
-		traseu.computeMaxAlt();
-		traseu.computeMinAlt();
-		traseu.printTrackData();
-		
-		db.insertTrackIntoDatabase(traseu);
+
+		Scanner trackFileScanner = new Scanner(new FileReader(trackFile));
+
+		while (trackFileScanner.hasNext()) {
+
+			String filename = trackFileScanner.next();
+			System.out.println(filename);
+			File file = new File(filename);
+			Track traseu = new Track();
+			XMLParser parser = new XMLParser(file);
+			Database db = new Database("MountainTracker.s3db");
+
+			parser.parse(traseu);
+
+			traseu.computeMaxAlt();
+			traseu.computeMinAlt();
+			traseu.printTrackData();
+
+			db.insertTrackIntoDatabase(traseu);
+		}
+
+		trackFileScanner.close();
 	}
 
 }
